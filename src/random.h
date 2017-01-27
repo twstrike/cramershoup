a24b82ad87a061ec1753b6c2db2c97cc838e9d97
Reinaldo de Souza Jr <juniorz@gmail.com>
2017-01-27 01:16:46 -0500

    Disable VERY_STRONG random with -DFAST_RANDOM
    Example:
      ./configure CFLAGS="-DFAST_RANDOM"

--- src/random.h
+++ src/random.h
@@ -6,7 +6,11 @@
 static void inline
 random_bytes_long_term(void * const buf, const size_t size) {
   //TODO: GCRY_VERY_STRONG_RANDOM is blocking, need to resolve this.
+#ifndef FAST_RANDOM
   gcry_randomize(buf, size, GCRY_VERY_STRONG_RANDOM);
+#else
+  gcry_randomize(buf, size, GCRY_STRONG_RANDOM);
+#endif
 }
 
 static void inline
